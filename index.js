7957a70f7b897610dd0069c930d4b56c812ba13a
Ankit Jangir <ankitjangirnice@gmail.com>
2022-06-13 11:55:58 +0530

    minor changes according to frontend demands

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 connectToMongo();
 
 //middleware to read json data
+app.use(cors());
 app.use(express.json());
 
 //availbale routes
@@ -12,5 +14,5 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNotebook app listening on port ${port}`)
 })
