ed3abe080565585241da43008981ab6f7b98608f
Ankit Jangir <ankitjangirnice@gmail.com>
2022-06-15 17:02:11 +0530

    all operations are perfectly working

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,17 +1,17 @@
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = "keepItHighly$ecure";
 
-const fetchuser = (req, res, next) => {
+const fetchuser = async (req, res, next) => {
     //get the user from jwt token and id to req object
     const token = req.header('auth-token');
     if (!token) {
-        res.status(401).send({ error: "please authinticate using a valid token" });
+        res.status(401).send({ error: "invalid jwt" });
     }
     try {
         const data = jwt.verify(token, JWT_SECRET);
-        req.user = data.user;
+        req.user = await data.user;
     } catch (error) {
-        res.status(401).send({ error: "please authinticate using a valid token" });
+        res.status(401).send({ error: "invalid jwt" });
     }
 
     next();
